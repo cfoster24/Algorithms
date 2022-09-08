34196ee546a3b878c6eca04c233c0c42fcc33411
cfoster24 <cfoster24@CSC20.cornellcollege.edu>
2022-09-08 09:54:21 -0500

    fixed heapsort

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -10,40 +10,65 @@ package heap;
  * @author cfoster24
  */
 public class Heap {
-    public static void sort(Comparable[] pq)
+    public static void sort(int[] pq)
     {
         int N = pq.length;
-        for (int k = N/2; k >= 1; k--)
-            sink(pq, k, N);
-        while(N > 1)
+        // build heap
+        for (int i = N/2 - 1; i >= 0; i--)
+            sink(pq, i, N);
+        
+        
+        for (int i = N - 1; i >= 0; i--)
         {
-            exch(pq, 1, N);
-            sink(pq, 1, --N);
+            exch(pq, 0, i);
+            sink(pq, 0, i);
         }
     }
     
-    private static void sink(Comparable[] pq, int k, int n) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && less(pq, j, j+1)) j++;
-            if (!less(pq, k, j)) break;
-            exch(pq, k, j);
-            k = j;
+    private static void sink(int[] pq, int k, int n) {
+        int largest = k; // initialize largest as root
+        int l = 2*k + 1; // left
+        int r = 2*k + 2; // right
+        
+        // If left child is larger than root
+        if (l < n && pq[l] > pq[largest])
+            largest = l;
+        
+        // If right child is largest so far
+        if (r < n && pq[r] > pq[largest])
+            largest = r;
+        
+        // If largest is not root
+        if (largest != k)
+        {   
+            
+            exch(pq, k, largest);
+            
+            // Repeatedly sink until largest is at the root
+            sink(pq, n, largest);
         }
-    }
+    } // sink(int[], int, int)
+   
     
-    private static boolean less(Comparable[] pq, int i, int j) {
-        return pq[i-1].compareTo(pq[j-1]) < 0;
-    }
-    
-    private static void exch(Object[] pq, int i, int j) {
-        Object swap = pq[i-1];
-        pq[i-1] = pq[j-1];
-        pq[j-1] = swap;
-    }
+    private static void exch(int[] pq, int i, int j) {
+        //swap the indexes of two items in an array
+        int swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    } // exch(int[], int, int)
     
     public static void main(String[] args)
     {
+        int a[] = {12, 11, 13, 5, 6, 7};
+        int n = a.length;
         
+        for(int i : a)
+            System.out.println(i);
+        System.out.println();
+        
+        Heap.sort(a);
+        
+        for(int i : a)
+            System.out.println(i);
     }
 }
