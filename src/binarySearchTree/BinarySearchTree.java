da6c9f92eff18b1a0666af2946c4ce00e7cd4698
cfoster24 <cfoster24@CSC20.cornellcollege.edu>
2022-09-13 10:21:55 -0500

    Added Red-Black tree functionality

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -10,18 +10,68 @@ package binarySearchTree;
 public class BinarySearchTree<Key extends Comparable<Key>, Value>{
     private Node root;
     
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    
     private class Node{
         private Key key;
         private Value val;
         private Node left, right;
         private int size;
+        private boolean color;
         
-        public Node(Key key, Value val, int size){
+        public Node(Key key, Value val, int size, boolean color){
             this.key = key;
             this.val = val;
             this.size = size;
+            this.color = color;
         }// Node(Key, Value, int)
     }// Node
+    
+    public boolean isEmpty(){
+        return root == null;
+    }// isEmpty()
+    
+    private boolean isRed(Node x){
+        if (x == null)
+            return false;
+        return x.color == RED;
+    }// isRed(Node)
+    
+    private Node rotateLeft(Node h){
+        assert isRed(h.right);
+        
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        
+        return x;
+    }// rotateLeft(Node)
+    
+    private Node rotateRight(Node h){
+        assert isRed(h.left);
+        
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        
+        return x;
+    }// rotateRight(Node)
+    
+    private void flipColors(Node h){
+        assert !isRed(h);
+        assert isRed(h.left);
+        assert isRed(h.right);
+        
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+        
+    }// flipColors(Node)
     
     public int size(){
         return size(root);
@@ -46,14 +96,19 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
 
         return null;  
     }// get(Key)
-
+    
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }// contains(Key)
+    
     public void put(Key key, Value val){
         root = put(root, key, val);
+        root.color = BLACK;
     }// put(Key, Value)
 
     private Node put(Node x, Key key, Value val){
         if (x == null) 
-            return new Node(key, val, 1);
+            return new Node(key, val, 1, RED);
         int cmp = key.compareTo(x.key);
 
         if(cmp < 0)
@@ -62,6 +117,14 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
             x.right = put(x.right, key, val);
         else
             x.val = val;
+        
+        if (isRed(x.right) && !isRed(x.left))
+            x = rotateLeft(x);
+        if (isRed(x.left) && isRed(x.left.left))
+            x = rotateRight(x);
+        if (isRed(x.left) && isRed(x.right))
+            flipColors(x);
+        
         x.size = 1 + size(x.left) + size(x.right);
         return x;
     }// put(Node, Key, Value)
@@ -99,7 +162,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
     }// inorder(Node, Queue<Key>)
     
     public void deleteMin(){
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
         root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
     }// deleteMin()
     
     private Node deleteMin(Node x){
@@ -111,8 +178,32 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
         return x;
     }// deleteMin(Node)
     
+    public void deleteMax(){
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = deleteMax(root);
+        if (!isEmpty()) root.color = BLACK;
+    }// deleteMax()
+    
+    private Node deleteMax(Node x){
+        if (x.right == null)
+            return x.right;
+        x.right = deleteMax(x.right);
+        x.size = 1 + size(x.left) + size(x.right);
+        
+        return x;
+    }// deleteMax(Node)
+    
     public void delete(Key key){
+        if (!contains(key)) return;
+
+         // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
         root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
     }// delete(Key)
     
     private Node delete(Node x, Key key){
