5cfd4cf86971b976beebcde61ad3ea315f0df8aa
cfoster24 <cfoster24@CSC20.cornellcollege.edu>
2022-09-20 13:28:32 -0500

    Imported Node to use in linked list.

---
+++ src/hashTable/Node.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashTable;
+
+import deque.*;
+
+/**
+ *
+ * @author cfoster24
+ * @param <Item>
+ */
+        
+public class Node<Item>{
+    
+    
+    private Node prev;
+    private Node next;
+    private Item payload;
+    // construct an empty deque
+    
+    public Node ( Node prev, Node next, Item payload) {
+        this.prev = prev;
+        this.next = next;
+        this.payload = payload;
+    } // Node(Node, Node, Item)
+    
+    public Node (Node next, Item payload) {
+        this.next = next;
+        this.payload = payload;
+    } // Node(Node, Item)
+     
+    public Node (Item payload) {
+        this.payload = payload;
+    } // Node(Item)
+
+    /**
+     * @return the prev
+     */
+    public Node getPrev() {
+        return prev;
+    } // getPrev()
+
+    /**
+     * @param prev the prev to set
+     */
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    } // setPrev(Node)
+
+    /**
+     * @return the next
+     */
+    public Node getNext() {
+        return next;
+    } // getNext()
+
+    /**
+     * @param next the next to set
+     */
+    public void setNext(Node next) {
+        this.next = next;
+    } // setNext(Node)
+
+    /**
+     * @return the payload
+     */
+    public Item getPayload() {
+        return payload;
+    } // getPayload()
+}
